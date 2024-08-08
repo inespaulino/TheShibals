e8c0907843a53c220212d01082691b16fec2b068
Inês Paulino <ines.paulino@hotmail.com>
2024-08-08 08:41:20 +0100

    adding background music

--- P2js/view/backView.js
+++ P2js/view/backView.js
@@ -1,41 +1,25 @@
 function render() {
     
-	const background = document.querySelector('#p2div');
-	const body = document.querySelector('#p2leftbody') || document.querySelector('#p2rightbody') || document.querySelector("#p2frontbody");
-    console.log("render",background);
-    body.innerHTML='';
-    const lol = $('#p2leftbody') || $('#p2rightbody');
-    lol.attr('id','p2backbody');
+	const div = document.querySelector('#p2div');
+	div.innerHTML = '';
+    const lol = document.getElementById('p2leftbody') || document.getElementById('p2rightbody') || document.getElementById("p2frontbody");
+    lol.id = 'p2backbody';
 
 	const item = document.createElement('div');
 
 	item.innerHTML = `<div>
         <img id="chair_img" src="/resources/chair.png"> 
-        <img id="arrow_left" src="/resources/arrowleft.png"> 
-        <img id="arrow_right" src="/resources/arrowright.png"> 				
+		<a href="#/player2leftside">
+       		<img id="arrow_right" src="/resources/arrowright.png" alt="Arrow Right"/>
+    	</a>
+
+		<a href="#/player2rightside">
+       		<img id="arrow_left" src="/resources/arrowleft.png" alt="Arrow Left"/>
+    	</a>				
 	</div>`
 	
-	background.appendChild(item);
-    body.appendChild(background);
+	div.appendChild(item);
 
-	console.log('entrou no render');
-	
-    const button = $('#arrow_left');
-    const button2 = $('arrow_right');
-	
-
-	button.on('click', function(){
-		console.log("hola");
-		
-		console.log(button.attr('src'));
-		
-	});
-    button2.on('click', function(){
-		console.log("hola");
-		
-		console.log(button2.attr('src'));
-		
-	});
 }
 
 export default { render };
